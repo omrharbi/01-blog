f365260632102d4b741f8cec2b53080dddd80ce9
omrharbi <rharbuomar@gmail.com>
2025-09-02 14:50:09 +0100

    set up backend spring boot

---
+++ backend/01-blog/src/main/java/com/__blog/Application.java
@@ -0,0 +1,14 @@
+package com.__blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("helleo");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
