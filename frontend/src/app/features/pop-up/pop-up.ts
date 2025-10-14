4f93a28210dff801d1773a242519721dfcf19e9a
omrharbi <rharbu.omar@gmail.com>
2025-10-14 12:37:08 +0100

    handle login

---
+++ frontend/src/app/features/pop-up/pop-up.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up',
+  imports: [],
+  templateUrl: './pop-up.html',
+  styleUrl: './pop-up.scss'
+})
+export class PopUp {
+
+}
--- frontend/src/app/features/posts/post-card/post-card.ts
+++ frontend/src/app/features/posts/post-card/post-card.ts
@@ -1,15 +1,20 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { Materaile } from '../../../modules/materaile-module';
 import { apiUrl } from '../../../core/constant/constante';
+import { PopUp } from '../../pop-up/pop-up';
 
 @Component({
   selector: 'app-post-card',
-  imports: [Materaile],
+  imports: [Materaile,PopUp],
   templateUrl: './post-card.html',
   styleUrl: './post-card.scss'
 })
 export class PostCard {
   apiUrl = apiUrl
- @Input() post: any;
-  
+  @Input() post: any;
+  show = false;
+  popUp() {
+    this.show = !this.show;
+    console.log("hwwe",this.show);
+  }
 }
