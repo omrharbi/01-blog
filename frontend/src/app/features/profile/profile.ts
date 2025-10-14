15778c79f13abe3741ae42a3053bf38acfc82f67
omrharbi <rharbu.omar@gmail.com>
2025-10-14 13:42:12 +0100

    handle login

--- frontend/src/app/features/profile/profile.ts
+++ frontend/src/app/features/profile/profile.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { EditProfile } from './edit-profile/edit-profile';
+import { Materaile } from '../../modules/materaile-module';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [EditProfile,Materaile],
   templateUrl: './profile.html',
   styleUrl: './profile.scss'
 })
 export class Profile {
-
+  editProfile = false;
+  EditPorfile() {
+    this.editProfile = !this.editProfile;
+  }
 }
