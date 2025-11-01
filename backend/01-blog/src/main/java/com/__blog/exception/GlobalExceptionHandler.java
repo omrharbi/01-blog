634d766d29499a2f346125d539568212a9efde9a
omrharbi <rharbu.omar@gmail.com>
2025-11-01 14:19:30 +0100

    report posts

--- backend/01-blog/src/main/java/com/__blog/exception/GlobalExceptionHandler.java
+++ backend/01-blog/src/main/java/com/__blog/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,6 +18,12 @@ public class GlobalExceptionHandler {
         response.put("error", ex.getMessage());
         return new ResponseEntity<>(response, ex.getHttpStatus());
         // return ResponseEntity.status(ex.getHttpStatus()).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<String> handleMethodNotAllowed(HttpRequestMethodNotSupportedException ex) {
+        String message = "Method " + ex.getMethod() + " is not allowed for this endpoint. Allowed method(s): ";
+        return ResponseEntity.status(405).body(message);
     }
 
     @ExceptionHandler(Exception.class)
