e4b1f06ba76ce768b0c1c90b6ecc5dcb8effdfed
omrharbi <rharbu.omar@gmail.com>
2025-11-04 13:05:22 +0100

    pageable get users

--- frontend/src/app/features/admin/ban-popup/ban-popup.ts
+++ frontend/src/app/features/admin/ban-popup/ban-popup.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { Materaile } from '../../../modules/materaile-module';
-import { ActionType } from '../../../core/models/admin/UserResponseInAdmin';
+import { ActionType, UserResponseInAdmin } from '../../../core/models/admin/UserResponseInAdmin';
 import { AdminService } from '../../../core/service/servicesAPIREST/admin/admin-service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-ban-popup',
@@ -15,6 +16,9 @@ export class BanPopup {
   @Output() close = new EventEmitter<void>();
   @Output() confirm = new EventEmitter<number>();
   @Input() actionType: ActionType = 'ban';
+  // allUsers = signal<UserResponseInAdmin[]>([]);
+  updateUser = new BehaviorSubject<UserResponseInAdmin | null>(null)
+  updateUser$ = this.updateUser.asObservable();
   step: number = 1;
   banDays: number = 7;
   quickOptions: number[] = [1, 7, 14, 30, 90];
@@ -35,10 +39,10 @@ export class BanPopup {
 
   confirmBan() {
     this.confirm.emit(this.banDays);
-    // console.log(this.banDays, "days", this.userId, "user id ");
-    this.adminService.banUser(this.userId, this.banDays).subscribe({
+     this.adminService.banUser(this.userId, this.banDays).subscribe({
       next: response => {
-        console.log(response, "response ban user");
+        this.updateUser.next(response.data)
+        // console.log(response, "response ban user");
       },
       error: error => {
         console.log(error, "error ");
