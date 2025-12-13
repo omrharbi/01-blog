0fffe9fbcd06d7a27796f33347840b7e7de6a448
omrharbi <rharbu.omar@gmail.com>
2025-12-13 11:22:37 +0100

    last version

--- backend/01-blog/src/main/java/com/__blog/security/UserPrincipal.java
+++ backend/01-blog/src/main/java/com/__blog/security/UserPrincipal.java
@@ -11,64 +11,64 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.__blog.model.entity.User;
 
-public class UserPrincipal implements UserDetails {
+    public class UserPrincipal implements UserDetails {
 
-    private final User user;
-    // private final boolean status;
+        private final User user;
+        // private final boolean status;
 
-    public UserPrincipal(User user  ) {
-        this.user = user;
-        // this.status = status;
+        public UserPrincipal(User user  ) {
+            this.user = user;
+            // this.status = status;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            List<SimpleGrantedAuthority> role = new ArrayList<>();
+            role.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())); 
+            return role;
+        }
+
+        @Override
+        public String getPassword() {
+            return user.getPassword();
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        @Override
+        public String getUsername() {
+            return user.getUsername();
+        }
+
+        public UUID getId() {
+            return user.getId();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
+        
+        public boolean isBanned() {
+            return user.isHidden();
+        }
+
     }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<SimpleGrantedAuthority> role = new ArrayList<>();
-        role.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())); 
-        return role;
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getUsername();
-    }
-
-    public UUID getId() {
-        return user.getId();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    
-    public boolean isBanned() {
-        return user.isHidden();
-    }
-
-}
--- backend/01-blog/src/main/java/com/__blog/webSocket/WebsocketConfig.java
+++ backend/01-blog/src/main/java/com/__blog/webSocket/WebsocketConfig.java
@@ -30,27 +30,29 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     private final WebSocketAuthInterceptor webSocketAuthInterceptor;// ths check the authentication of the user
 
     @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(webSocketAuthInterceptor); // ← THIS IS CRITICAL! for authentication
+    }
+
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {// ← THIS IS CRITICAL! for CORS
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("http://localhost:4200")
                 .withSockJS();
     }
 
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(webSocketAuthInterceptor); // ← THIS IS CRITICAL! for authentication
-    }
-
-    @Override// configure message broker for routing messages from server to client and vice versa 
+    @Override // configure message broker for routing messages from server to client and vice
+              // versa
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/user", "/topic");
         registry.setApplicationDestinationPrefixes("/app");
         // registry.setUserDestinationPrefix("/user");
     }
 
-    @Override // to access the authenticated user in the controller methods 
+    @Override // to access the authenticated user in the controller methods
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver()); // this for get the authenticated user in the controller methods
+        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver()); // this for get the authenticated user in
+                                                                              // the controller methods
     }
 
     @Override // to convert messages to/from JSON
